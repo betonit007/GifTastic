752533e7aff5e096cedc7ebb208c9adc5f08b9bc
betonit007 <timnagorski@hotmail.com>
2018-10-04 14:09:20 -0400

    good progress

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,40 +1,104 @@
 //////Variables/////////////
 var userSearchTerm;
-var termArray = [];
+var topics = ["drones", "football", "fortnite", "saab", "robotics", "Star Wars", "Trains"];
+var videoReturnAmount = 10;
+var video;
+var image;
+var getVid;
+
+//////create buttons////////////////
+for (var i = 0; i < topics.length; i++) {
+       
+    $("#buttonField").append("<button id=" + topics[i] + " class='topicBut' value =" + topics[i] + ">" + topics[i] + "</button>");
+}
+
 
 $(document).ready(function() {
+
+    $(document).on("click", "button.topicBut", function() {
+        $("#videoField").empty();
+        var clickValue = $(this).text();
+
+        console.log(clickValue);
+
+
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + clickValue + "&api_key=dc6zaTOxFJmzC&limit=&limit=" + videoReturnAmount + "";
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+          }).then(function(response) {
+
+
+         for (var i = 0; i < videoReturnAmount; i++) {
+      
+           var video = response.data[i].images.original.url;
+
+           var image = video.replace("giphy.gif", "480w_s.jpg");
+         
+           $("#videoField").append("<img src=" + video + ">");
+
+          }
+
+
+        });
+        
+  
+      });
+
+
+
     /////search click/////////
    $("#buttonSearch").on("click", function(event) {
-    console.log("ok...");
-    userSearchTerm = $("#userInput").val();
-    termArray.push(userSearchTerm);
-    console.log(termArray);
+     userSearchTerm = $("#userInput").val();
 
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userSearchTerm + "&api_key=dc6zaTOxFJmzC";
+     topics.push(userSearchTerm);
+
+     $("#buttonField").empty();
     
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-
-      $("#buttonField").append("<button id=" + userSearchTerm + " value =" + userSearchTerm + ">" + userSearchTerm + "</button>");
-   
-      var video = response.data[0].images.original.url;
-
-      var newStr = video.replace("giphy.gif", "480w_s.jpg");
-  
-      $("#videoField").html("<img src=" + newStr + ">");
-
+     //////add search term to array/////
+     for (var i = 0; i < topics.length; i++) {
+       
+        $("#buttonField").append("<button id=" + topics[i] + " class='topicBut' value =" + topics[i] + ">" + topics[i] + "</button>");
+     
+     }
 
     });
 
 
     
 
+
+     //var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + userSearchTerm + "&api_key=dc6zaTOxFJmzC&limit=&limit=" + videoReturnAmount + "";
+
+       // $.ajax({
+         //  url: queryURL,
+         //  method: "GET"
+         //  }).then(function(response) {
+         //  console.log(response);
+
+       
+
+       
+
+         //for (var i = 0; i < videoReturnAmount; i++) {
+      
+          // var video = response.data[i].images.original.url;
+
+          // var image = video.replace("giphy.gif", "480w_s.jpg");
+         
+          // $("#videoField").html("<img src=" + video + ">");
+
+          //}
+
+
+       // });
+    
+
+    
+
    });
 
-});
+
 
 
--- assets/javascript/replacestring.js
+++
@@ -1,9 +0,0 @@
-
-
-function strReplace(){
-        var myStr = 'quick_brown_fox';
-        var newStr = myStr.replace(/_/g, "-");
-        
-        // Insert modified string in paragraph
-        document.getElementById("myText").innerHTML = newStr;
-    }
